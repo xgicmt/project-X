3dfd043591cf81ad938cea437c2877cb3b21a91b
xgicmt <xgicmt@gmail.com>
2015-09-01 22:18:02 -0500

    Charge Style update, jquery for checkbox submit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,45 +17,52 @@
 //= require_tree .
 
 
-jQuery(function(){
+  jQuery(function(){
   	$('#disclaimBtn').click(function(){
 	  $('#disclaimer').slideToggle(500);
 	});
 
-	$('.flash:visible').delay(5000).slideUp();
+  	$('.flash:visible').delay(5000).slideUp();
+	  $('.userName').click(function(){
+	    $('.underName').slideToggle(500);
+	    $('.userName').css("border-radius", "5px 5px 0 0");
+	  });
+  });
 
-	$('.userName').click(function(){
-	  $('.underName').slideToggle(500);
-	  $('.userName').css("border-radius", "5px 5px 0 0");
-	});
-});
-
-$(document).ready(function(){
+  $(document).ready(function(){
     $('.carousel').carousel();
-});
+  });
 
-MarkdownToolbar.buttons = [
-  {title: 'Bold',          type: 'wrapper',  left: '**', right: '**', id: 'bold'},
-  {title: 'Italic',        type: 'wrapper',  left: '*',  right: '*',  id: 'italic'},
-  {id: 'delimiter'},
-  {title: 'H1',            type: 'wrapper',  left: '# ', right: ' #', id: 'heading_1'},
-  {title: 'H2',            type: 'wrapper',  left: '# ', right: ' #', id: 'heading_2'},
-  {title: 'H3',            type: 'wrapper',  left: '# ', right: ' #', id: 'heading_3'},
-  {id: 'delimiter'},
-  {title: 'Bulleted list', type: 'prefixer', left: '- ',              id: 'list_bullets'},
-  {title: 'Numbered list', type: 'list_numbers', id: 'list_numbers'},  
-  {title: 'Blockquote',    type: 'prefixer', left: '> ',              id: 'blockquote'},
-  // Code:
-  {title: 'Source Code',   type: 'block_wrapper',   left: "```\n", right: "\n```", id: 'code'},  
-  {id: 'delimiter'},
-  {title: 'Link',          type: 'link',         id: 'link'},
-];
-$('#propimageUpload').change(function(){
+  MarkdownToolbar.buttons = [
+    {title: 'Bold',          type: 'wrapper',  left: '**', right: '**', id: 'bold'},
+    {title: 'Italic',        type: 'wrapper',  left: '*',  right: '*',  id: 'italic'},
+    {id: 'delimiter'},
+    {title: 'H1',            type: 'wrapper',  left: '# ', right: ' #', id: 'heading_1'},
+    {title: 'H2',            type: 'wrapper',  left: '# ', right: ' #', id: 'heading_2'},
+    {title: 'H3',            type: 'wrapper',  left: '# ', right: ' #', id: 'heading_3'},
+    {id: 'delimiter'},
+    {title: 'Bulleted list', type: 'prefixer', left: '- ',              id: 'list_bullets'},
+    {title: 'Numbered list', type: 'list_numbers', id: 'list_numbers'},  
+    {title: 'Blockquote',    type: 'prefixer', left: '> ',              id: 'blockquote'},
+    // Code:
+    {title: 'Source Code',   type: 'block_wrapper',   left: "```\n", right: "\n```", id: 'code'},  
+    {id: 'delimiter'},
+    {title: 'Link',          type: 'link',         id: 'link'},
+  ];
+  $('#propimageUpload').change(function(){
     if(this.files.length>10)
         alert('to many files')
-});
+  });
 // Prevent submission if limit is exceeded.
-$('.edit_property').submit(function(){
+  $('.edit_property').submit(function(){
     if(this.files.length>10)
-        return false;
-});
+      return false;
+  });
+
+   $(document).ready(function(){
+    $('#policyCheck').click(function(){
+      $('#scriptDiv').toggle();
+    });
+   });
+
+
